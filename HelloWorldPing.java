984efe23f2331353a96eb3ce19713af709672ad1
vishvas-git <42351361+vishvas-git@users.noreply.github.com>
2020-04-11 18:49:49 +0530

    Update HelloWorldPing.java

--- HelloWorldPing.java
+++ HelloWorldPing.java
@@ -1,6 +1,6 @@
 public class HelloWorldPing {
 	public static void main(String[] args) throws InterruptedException {
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("Java->GitHub->DockerHub(CI)|Hello World! " + i);
 			Thread.sleep(1000);
 		}
